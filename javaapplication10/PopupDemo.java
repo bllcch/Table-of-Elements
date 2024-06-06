96d40bf7d2804ea8e35fa1f4db9a716f729c779e
Belle R. Cacho <162699672+bllcch@users.noreply.github.com>
2024-06-07 01:18:19 +0700

    Add files via upload

---
+++ javaapplication10/PopupDemo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author ACER
+ */
+public class PopupDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PopupFrame frame = new PopupFrame();
+         frame.show(); 
+        
+        // TODO code application logic here
+    }
+    
+}
